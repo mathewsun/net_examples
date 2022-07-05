d9305c19e69bbe263ee10154ec7afd15838ee971
Matvey D <mathewsun@rambler.ru>
2022-07-05 21:00:10 +0300

    Polymorphism example

---
+++ NetExamples/Polymorphism/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace Polymorphism
+{
+    internal class Program
+    {
+
+        class A
+        {
+            public void Publish() { }
+            public virtual void Print() { Console.WriteLine("Print A"); }
+        }
+
+        class B : A
+        {
+            public override void Print()
+            {
+                Console.WriteLine("Print B");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            A parentElement = new B();
+
+            parentElement.Print();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
